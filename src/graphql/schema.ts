83e9d54eacbf52585740590f85f9417bdac5f672
Halyson Rezende <halyson97@gmail.com>
2018-08-08 16:58:52 -0300

    create token

--- src/graphql/schema.ts
+++ src/graphql/schema.ts
@@ -10,10 +10,13 @@ import { commentTypes } from './resources/comment/comment.schema';
 import { commentResolvers } from './resources/comment/comment.resolvers';
 import { postResolvers } from './resources/post/post.resolvers';
 import { userResolvers } from './resources/user/user.resolvers';
+import { tokenTypes } from './resources/token/token.schema';
+import { tokenResolvers } from './resources/token/token.resolvers';
 
 const resolvers =  merge(
     commentResolvers,
     postResolvers,
+    tokenResolvers,
     userResolvers
 );
 
@@ -31,7 +34,8 @@ export default makeExecutableSchema({
         Mutation,
         userTypes,
         postTypes,
-        commentTypes
+        commentTypes,
+        tokenTypes
     ],
     resolvers
 });
